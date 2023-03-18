257c4a671b6578064d58ad3674c037c6319ab03c
Mr-Psmith <psancthus@gmail.com>
2023-03-18 09:59:36 -0100

    moved conditional content solutions to note

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -39,49 +39,7 @@ function Expenses(props) {
           onChangeFilter={filterChangeHandler}
         /> 
         {expensesContent}
-        {/*>_< it was props.item.map() instead filteredExpenses.map() but the filter exchanged */}
-        {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>} */} {/* This is the same as below only we are abusing a js "trick"(said Max)which is: if the first condition is met, than with && js returns the part after the and operator 
-        {/* {filteredExpenses.length === 0 ? (<p>No expenses found.</p>) : ( /* This is a turnary expression for the conditional expression 
-          filteredExpenses.map((expense) => ( /* map("I want to map every expense in my expenses array into an expense item JSX element.) So I want to transform the expense object to this special kind of object, to this JSX element
-              <ExpenseItem /* And after this we can configure like we did before, but expense has passed as a parameter into this function automatically, because that's how map works, this expense is used to extract title. This is why we call expense.title only 
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date} /> )))} */}
-         {/* I did copy the below part from abow bec max rewrote it. this is the second the third is what we use  */}
-       {/*  {filteredExpenses.length > 0 &&  
-          filteredExpenses.map((expense) => ( 
-              <ExpenseItem 
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-              />
-            ))} */}
-        {/* DELETED when the above .map()solution was made */}
-        {/* <ExpenseItem
-          title={props.items[0].title} //we could hardcode too like: title="Toilet Paper"
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        /> */}
       </Card>
     </div>
   );
 }
-
-export default Expenses;
